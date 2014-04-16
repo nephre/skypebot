a5defc36c30b1fdaf9aab78ac94dcfe831855bd1
Daniel Jeznach <daniel.jeznach@smtsoftware.com>
2014-04-16 10:12:00 +0200

    Harvester functionality added

---
+++ Harvester/AbstractHarvester.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * AbstractHarvester class container 
+ *
+ * @package     R-Infiniti
+ * @version     $Id$
+ * @copyright   2014 SMT Software S.A.
+ * @filesource
+ */
+/**
+ * {descripton}
+ *
+ * @package     R-Infiniti
+ * @author      Daniel Jeznach <daniel.jeznach@smtsoftware.com>
+ */
+class AbstractHarvester 
+{
+
+}
